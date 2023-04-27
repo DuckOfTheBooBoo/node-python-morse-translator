83eed14baec7ba3aeb2951c40c649e536500aa7c
Arajdian Altaf <74168834+DuckOfTheBooBoo@users.noreply.github.com>
2023-04-27 19:11:06 +0700

    Change server port to 80 HTTP

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ const init = async () => {
 
   // SERVER
   const server = Hapi.server({
-    port: 9000,
+    port: 80,
     host: process.env.NODE_ENV !== 'prod' ? 'localhost' : '0.0.0.0',
     routes: {
       files: {
