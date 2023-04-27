264b864c403b49fa7b77a3def11fd401cf03466e
Arajdian Altaf <arajdianaltaf123@gmail.com>
2023-04-27 14:20:46 +0700

    Added flask API

--- src/python_api/api.py
+++ src/python_api/api.py
@@ -1,10 +1,32 @@
-import json
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS
+from play_morse import play_morse
+from io import BytesIO
+import json, base64
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/generateMorseTone")
+@app.route("/", methods=['GET'])
 def generateMorseTone():
-    return '<h1>Hello from Flask'
 
-app.run(host='localhost', port=9001)
+    # Query Parameters
+    args = request.args
+    if 'data' in args.keys():
+        audio = play_morse(args["data"])
+        audio_stream = base64.b64encode(audio).decode('utf-8')
+        return json.dumps({
+            'data': "data:audio/wav;base64," + audio_stream,
+        }), 200
+
+
+    else:
+        response = json.dumps({
+            "message": "Error, no data query provided."
+        })
+
+        return response, 400
+    
+    
+if __name__ == '__main__':
+    app.run(host='localhost', port=9001)
